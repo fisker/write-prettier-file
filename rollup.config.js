d0630a40a2c3033d8474db2d1aa5cb8f9d58b758
fisker <lionkay@gmail.com>
2019-12-03 10:56:06 +0800

    feat: convert project to esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import babel from 'rollup-plugin-babel'
 
 export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs',
-  },
+  input: 'src/index.mjs',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'esm',
+    },
+  ],
   plugins: [babel()],
 }
